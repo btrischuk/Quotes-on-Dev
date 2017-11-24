55158857467566bd47f7c380b7debd4a5892f647
btrischuk <b-trischuk@hotmail.com>
2017-11-24 10:14:16 -0800

    fix js bug and made buttons work

--- js/api.js
+++ js/api.js
@@ -1,85 +1,83 @@
 (function($) {
 
+
     $( '.new-quote-button' ).on( 'click', function (e) {
       e.preventDefault();
-      console.log('hiiiii');
-      alert('heeloooo');
       
-      // $('#quote-content').empty();  
-      // $('#quote-title').empty();
+      $('#quote-content').empty();  
+      $('#quote-title').empty();
 
-      // $.ajax( {
-      //   method: 'GET',
-      //   url: api_vars.root_url + 'wp/v2/posts/?filter[orderby]=rand&filter[posts_per_page]=1',
-      // })
-      // .done( function(data) {
+      $.ajax( {
+        method: 'GET',
+        url: api_vars.root_url + 'wp/v2/posts/?filter[orderby]=rand&filter[posts_per_page]=1',
+      })
+      .done( function(data) {
 
-      //     var post = data.shift(); // The data is an array of posts. Grab the first one.
-      //     // history.pushState
-      //     $( '#quote-title' ).text( post.title.rendered );
-      //     $( '#quote-content' ).html( post.content.rendered );
-
-
+          var post = data.shift(); // The data is an array of posts. Grab the first one.
+          // history.pushState
+          $( '#quote-title' ).text( post.title.rendered );
+          $( '#quote-content' ).html( post.content.rendered );
 
 
 
-          // if (data._qod_quote_source){
-          // $('.source').html('<a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>');
-          // }
+
+
+          if (data._qod_quote_source){
+          $('.source').html('<a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>');
+          }
 
           // .fail
-              // console.log(post);
-        //   });
+          //     console.log(post);
+          // });
         });
       
-
+      });
       
 //submit new post
   
       $( '#submit-quote-button' ).on( 'click', function ( e ) {
 
         e.preventDefault();
-        console.log('hiiiii');
-        alert('heeloooo');
         
-      //   var quoteAuthor =$('#quote-author').val();
-      //   var quoteContent =$('#quote-content').val();
-      //   var quoteSource =$('#quote-source').val();
-      //   var quoteSourceUrl =$('#quote-source-url').val();
+        var quoteAuthor =$('#quote-author').val();
+        var quoteContent =$('#quote-content').val();
+        var quoteSource =$('#quote-source').val();
+        var quoteSourceUrl =$('#quote-source-url').val();
         
-      //   // console.log(quoteAuthor);
+        // console.log(quoteAuthor);
 
   
-      //   // $('#quote-content').empty();
-      //   // $('#quote-title').empty();
+        // $('#quote-content').empty();
+        // $('#quote-title').empty();
   
-      //   $.ajax( {
-      //     method: 'POST',
-      //     url: api_vars.root_url + 'wp/v2/posts',
-      //     data: {
-      //       title: quoteAuthor,
-      //       content: quoteContent,
-      //       _qod_quote_source: quoteSource,
-      //       _qod_quote_source_url: quoteSourceUrl,
-      //       status: 'publish'
-      //     },
+        $.ajax( {
+          method: 'POST',
+          url: api_vars.root_url + 'wp/v2/posts',
+          data: {
+            title: quoteAuthor,
+            content: quoteContent,
+            _qod_quote_source: quoteSource,
+            _qod_quote_source_url: quoteSourceUrl,
+            status: 'publish'
+          },
           
-      //     beforeSend: function(xhr) {
-      //       xhr.setRequestHeader( 'X-WP-Nonce', api_vars.nonce );
-      //     }
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader( 'X-WP-Nonce', api_vars.nonce );
+          }
           
-      //   }).done( function() {
+        }).done( function() {
       
-      //     // var post = data.shift(); // The data is an array of posts. Grab the first one.
-      //     // $( '#quote-title' ).text( post.title.rendered );
-      //     // $( '#quote-content' ).html( post.content.rendered );
+          // var post = data.shift(); // The data is an array of posts. Grab the first one.
+          // $( '#quote-title' ).text( post.title.rendered );
+          // $( '#quote-content' ).html( post.content.rendered );
           
-      //     //       // console.log(post);
-      //   }).always(function() {
-      //     $('#quote-submission-form').trigger('reset');
-      //   });
-      })
-    });(jQuery);
+          //       // console.log(post);
+        }).always(function() {
+          $('#quote-submission-form').trigger('reset');
+        });
+
+      }); // end of $( '#submit-quote-button' ).on( 'click'
+ 
   
   
 
@@ -109,3 +107,23 @@
       // } );
     // } );
   // } );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})(jQuery);
